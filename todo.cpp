760a134f6070f3f335d4149f5c4362e0f6962a84
Dokito555 <thinkingwhiterabbit@gmail.com>
2023-12-14 11:45:27 +0700

    finalization (might update & debug)

--- todo.cpp
+++ todo.cpp
@@ -40,10 +40,10 @@ void readTodo() {
 			// true -> [✓] = Done, 
 			// false -> [] = Not Done
 			if (TodoList[i].isDone) {
-				cout << "Done";
+				cout << "[✓]";
 			}
 			else {
-				cout << "Not Done";
+				cout << "[]";
 			}
 			cout << " > ";
 			cout << TodoList[i].dueDate;
@@ -63,27 +63,29 @@ void readTodo() {
 void readTodoFromId(int index) {
 	// Check if currentSize is below array capacity
 	if (currentSize < 101) {
-		// Print out specific todo
-		for (int i = 1; i < currentSize; i++) {
-			if (TodoList[i].id == index) {
-				cout << TodoList[i].id << ". ";
-				cout << TodoList[i].title << endl;
-				cout << endl;
-				cout << "description: " << endl;
-				cout << TodoList[i].desc << endl;
-				cout << endl;
-				cout << "Due Date: " << endl;
-				cout << TodoList[i].dueDate << endl;
-				cout << endl;
-				cout << "status: ";
-				// Check isDone status: 
-				// true -> [✓] = Done, 
-				// false -> [] = Not Done
-				if (TodoList[i].isDone) {
-					cout << "Done";
-				}
-				else {
-					cout << "Not Done" << endl;
+		if (index <= currentSize && index > 0) {
+			// Print out specific todo
+			for (int i = 1; i < currentSize; i++) {
+				if (TodoList[i].id == index) {
+					cout << TodoList[i].id << ". ";
+					cout << TodoList[i].title << endl;
+					cout << endl;
+					cout << "description: " << endl;
+					cout << TodoList[i].desc << endl;
+					cout << endl;
+					cout << "Due Date: " << endl;
+					cout << TodoList[i].dueDate << endl;
+					cout << endl;
+					cout << "status: ";
+					// Check isDone status: 
+					// true -> [✓] = Done, 
+					// false -> [] = Not Done
+					if (TodoList[i].isDone) {
+						cout << "[✓]";
+					}
+					else {
+						cout << "[]" << endl;
+					}
 				}
 			}
 		}
@@ -97,14 +99,51 @@ void readTodoFromId(int index) {
 }
 
 // Print out specific Todo from Todolist array by Date
-
+void readTodoFromDate(string date) {
+	if (currentSize < 101) {
+		if (validateDate(date)) {
+			for (int i = 1; i <= currentSize; i++) {
+				if (TodoList[i].dueDate == date) {
+					cout << TodoList[i].id << ". ";
+					cout << TodoList[i].title << endl;
+					cout << endl;
+					cout << "description: " << endl;
+					cout << TodoList[i].desc << endl;
+					cout << endl;
+					cout << "Due Date: " << endl;
+					cout << TodoList[i].dueDate << endl;
+					cout << endl;
+					cout << "status: ";
+					// Check isDone status: 
+					// true -> [✓] = Done, 
+					// false -> [] = Not Done
+					if (TodoList[i].isDone) {
+						cout << "[✓]";
+					}
+					else {
+						cout << "[]" << endl;
+					}
+				}
+			}
+		}
+		else {
+			cout << "Wrong date format please try again.." << endl;
+		}
+	}
+	// Error Handling: 
+	// check if currentSize above array capacity
+	// return message when triggered
+	else {
+		cout << "Todo Exceeded Maximum array capacity, Couldn't Read Todo" << endl;
+	}
+}
 
 // Update Todo Title
 string updateTodoTitle(int index, string newTitle) {
 	// Check if currentSize is below array capacity
 	if (currentSize < 101) {
 		// Check if index is below currentSize
-		if (index <= currentSize) {
+		if (index <= currentSize && index > 0) {
 			for (int i = 1; i < currentSize; i++) {
 				if (TodoList[i].id == index) {
 					TodoList[i].title = newTitle;
@@ -132,10 +171,38 @@ string updateTodoDescription(int index, string newDesc) {
 	// Check if currentSize is below array capacity
 	if (currentSize < 101) {
 		// Check if index is below currentSize
-		if (index <= currentSize) {
+		if (index <= currentSize && index > 0) {
 			for (int i = 1; i < currentSize; i++) {
 				if (TodoList[i].id == index) {
 					TodoList[i].desc = newDesc;
+					return "updated!";
+				}
+			}
+		}
+		// Error Handling: 
+		// check if index above array capacity
+		// return message when triggered
+		else {
+			return "id parameter above the current Todolist index, Couldn't Update Todo";
+		}
+	}
+	// Error Handling: 
+	// check if currentSize above array capacity
+	// return message when triggered
+	else {
+		return "Todo Exceeded Maximum array capacity, Couldn't Update Todo";
+	}
+}
+
+// Update Todo Due Date
+string updateTodoDueDate(int index, string newDate) {
+	// Check if currentSize is below array capacity
+	if (currentSize < 101) {
+		// Check if index is below currentSize
+		if (index <= currentSize && index > 0) {
+			for (int i = 1; i < currentSize; i++) {
+				if (TodoList[i].id == index) {
+					TodoList[i].dueDate = newDate;
 					return "updated!";
 				}
 			}
@@ -160,7 +227,7 @@ string toggleTodo(int index) {
 	// Check if currentSize is below array capacity
 	if (currentSize < 101) {
 		// Check if index is below currentSize
-		if (index <= currentSize) {
+		if (index <= currentSize && index > 0) {
 			for (int i = 1; i < currentSize; i++) {
 				if (TodoList[i].id == index) {
 					// Toggle IsDone
@@ -190,7 +257,7 @@ string deleteTodo(int index) {
 	// Check if currentSize is below array capacity and index number below currentSize
 	if (currentSize < 101) {
 		// Check if index is below currentSize
-		if (index <= currentSize) {
+		if (index <= currentSize && index > 0) {
 			for (int i = 1; i < currentSize; i++) {
 				if (TodoList[i].id == index) {
 					// Temporary: Change the deleted Todo to an empty element
@@ -218,15 +285,16 @@ string deleteTodo(int index) {
 int main() {
 	int opt;
 	Todo todo;
-	string status, exitOpt;
+	string status, exitOpt, dateArg;
 	do {
 		cout << "WELCOME TO TODO LIST" << endl;
 		cout << endl;
 		cout << "1. Insert Todo" << endl;
 		cout << "2. Read Todo" << endl;
 		cout << "3. Read Todo From Id" << endl;
-		cout << "4. Update Todo" << endl;
-		cout << "5. Delete Todo" << endl;
+		cout << "4. Read Todo From Date" << endl;
+		cout << "5. Update Todo" << endl;
+		cout << "6. Delete Todo" << endl;
 		cout << "9. Exit" << endl;
 		cout << "Pick your option: ";
 		cin >> opt;
@@ -255,17 +323,17 @@ int main() {
 				break;
 			}
 			cout << endl;
-			cout << "Is it done yet? y/n: ";
+			cout << "Is it done yet? [Y/N]: ";
 			status = "";
 			cin >> status;
-			if (status != "y" && status != "n") {
+			if (status != "y" && status != "n" && status != "Y" && status != "N") {
 				cout << "Wrong Answer format please try again" << endl;
 				return 0;
 			}
-			if (status == "y") {
+			if (status == "y" || status == "Y") {
 				todo.isDone = true;
 			}
-			if (status == "n") {
+			if (status == "n" || status == "N") {
 				todo.isDone = false;
 			}
 			todo.id = currentSize;
@@ -278,24 +346,24 @@ int main() {
 			cout << endl;
 			readTodo();
 			cout << endl;
-			cout << "Would you like to exit? y/n: ";
+			cout << "Would you like to exit? [Y/N]: ";
 			exitOpt = "";
 			cin >> status;
-			if (exitOpt != "y" && exitOpt != "n") {
+			if (exitOpt != "y" && exitOpt != "n" && exitOpt != "Y" && exitOpt != "N") {
 				cout << "Couldn't Read Option, Automatically exit..." << endl;
 				break;
 			}
-			if (exitOpt == "y") {
+			if (exitOpt == "y" || exitOpt == "Y") {
 				cout << "Proceed to exit" << endl;
 				cout << endl;
 				break;
 			}
-			if (exitOpt == "n") {
+			if (exitOpt == "n" || exitOpt == "N") {
 				cout << "On stand by" << endl;
 				cout << endl;
 			}
 		case 3:
-			// Initiate Read todo from id
+			// Initiate Read Todo from id
 			// Input id as argument
 			int readId;
 
@@ -305,22 +373,45 @@ int main() {
 			cin >> readId;
 			cout << endl;
 			readTodoFromId(readId);
-			cout << "Would you like to exit? y/n: ";
+			cout << "Would you like to exit? [Y/N]: ";
 			cin >> exitOpt;
-			if (exitOpt != "y" && exitOpt != "n") {
+			if (exitOpt != "y" && exitOpt != "n" && exitOpt != "Y" && exitOpt != "N") {
 				cout << "Couldn't Read Option, Automatically exit..." << endl;
 				break;
 			}
-			if (exitOpt == "y") {
+			if (exitOpt == "y" || exitOpt == "Y") {
 				cout << "Proceed to exit" << endl;
 				cout << endl;
 				break;
 			}
-			if (exitOpt == "n") {
+			if (exitOpt == "n" || exitOpt == "N") {
 				cout << "On stand by" << endl;
 				cout << endl;
 			}
 		case 4:
+			// Initiate Read Todo By Date
+			cout << "READ TODO FROM DATE" << endl;
+			cout << endl;
+			cout << "Which Date?: ";
+			cin >> dateArg;
+			cout << endl;
+			readTodoFromDate(dateArg);
+			cout << "Would you like to exit? [Y/N]: ";
+			cin >> exitOpt;
+			if (exitOpt != "y" && exitOpt != "n" && exitOpt != "Y" && exitOpt != "N") {
+				cout << "Couldn't Read Option, Automatically exit..." << endl;
+				break;
+			}
+			if (exitOpt == "y" || exitOpt == "Y") {
+				cout << "Proceed to exit" << endl;
+				cout << endl;
+				break;
+			}
+			if (exitOpt == "n" || exitOpt == "N") {
+				cout << "On stand by" << endl;
+				cout << endl;
+			}
+		case 5:
 			// Initiate update Todo
 			int updateId, updateOpt;
 			cout << "UPDATE TODO" << endl;
@@ -331,7 +422,9 @@ int main() {
 			cout << "What do you want to update?" << endl;
 			cout << "1. Title" << endl;
 			cout << "2. Description" << endl;
-			cout << "3. Status" << endl;
+			cout << "3. Due Date" << endl;
+			cout << "4. Status" << endl;
+			cout << "9. Exit" << endl;
 			cout << "Your option: ";
 			cin >> updateOpt;
 			cout << endl;
@@ -352,14 +445,24 @@ int main() {
 				cout << endl;
 				break;
 			case 3:
+				cout << "Write your new Due Date: ";
+				cin.get();
+				getline(cin, todo.dueDate);
+				cout << updateTodoDueDate(updateId, todo.dueDate);
+				cout << endl;
+				break;
+			case 4:
 				cout << toggleTodo(updateId);
 				cout << endl;
+				break;
+			case 9:
+				cout << "EXIT, Goodbye" << endl;
 				break;
 			default:
 				break;
 			}
 			break;
-		case 5:
+		case 6:
 			int deleteID;
 
 			cout << "DELETE TODO" << endl;
